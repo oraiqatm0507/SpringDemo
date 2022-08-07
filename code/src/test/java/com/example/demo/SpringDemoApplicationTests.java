a1e28c1cf6630e76b4ef66f4b04b9b9af9245547
oraiqatm <oraiqatm@mahmouds-MacBook-Pro.local>
2022-08-07 10:00:36 -0700

    update

---
+++ code/src/test/java/com/example/demo/SpringDemoApplicationTests.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	AppController app = new AppController();
+	
+	@Test
+	void contextLoads() {
+		Date date = new Date();
+		//HH:mm:ss 24 hour format
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh.mm aa");
+
+		// Use Madrid's time zone to format the date in
+		df.setTimeZone(TimeZone.getTimeZone("America/Chicago"));
+
+		String correctSol ="Date and Time for " + "UTC" + ": " + df.format(date) + "<br>";
+		
+		String testResult = app.GetLocalDateTime("UTC","America/Chicago");
+		
+		assert(testResult.equals(correctSol));
+		
+	}
+
+}

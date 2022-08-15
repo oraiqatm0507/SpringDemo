33bab9cf57593ad19a7b00fd430c9c6322e586d7
oraiqatm <oraiqatm@mahmouds-MacBook-Pro.local>
2022-08-15 10:55:16 -0700

    more

--- code/src/test/java/com/example/demo/SpringDemoApplicationTests.java
+++ code/src/test/java/com/example/demo/SpringDemoApplicationTests.java
@@ -30,4 +30,21 @@ class SpringDemoApplicationTests {
 		
 	}
 
+	@Test
+	void LosAngels() {
+		Date date = new Date();
+		//HH:mm:ss 24 hour format
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh.mm aa");
+
+		// Use Madrid's time zone to format the date in
+		df.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
+
+		String correctSol ="Date and Time for " + "UTC" + ": " + df.format(date) + "<br>";
+		
+		String testResult = app.GetLocalDateTime("UTC","America/Los_Angeles");
+		
+		assert(testResult.equals(correctSol));
+		
+	}
+
 }
